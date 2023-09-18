ca64879413d844effb08f698d0ca6bc1a558208b
Daniel Marques <danielmarianomarquespr@gmail.com>
2023-09-18 20:55:06 -0300

    Troca da string de olá para Olá Mundo

--- src/main/java/com/estudo/java/cap1/Algoritimo.java
+++ src/main/java/com/estudo/java/cap1/Algoritimo.java
@@ -2,6 +2,6 @@ package com.estudo.java.cap1;
 
 public class Algoritimo {
 	public static void main(String[] args) {
-        System.out.println("Olá!");
+        System.out.println("Olá Mundo!");
     }
 }
